42b50b4b6f7695289866049a599ae175a222358a
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-04-02 14:52:19 -0700

    remove old verialtor files

--- verilator/user-coverage/parse_cov.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-data = open('coverage.dat', 'rb').read()
-lines = [l[4:-3] for l in data.split(b"\n") if l.startswith(b"C '")]
-lines = [ {e[0]: e[1] for e in  (e.split(b"\x02") for e in l.split(b"\x01"))} for l in lines]
-for l in lines:
-  print(l)
--- verilator/user-coverage/test.v
+++
@@ -1,6 +0,0 @@
-module test(
-  input        clock,
-  input  [7:0] a,
-  input  [7:0] b
-);
-endmodule

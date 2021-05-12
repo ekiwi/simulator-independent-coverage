4a329feb3c1c89a4aa8943e01ab1d1c15017b887
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-05-11 18:24:48 -0700

    [coverage] split into FSM info collection and FSM instrumentation

--- coverage/src/coverage/FsmCoverage.scala
+++ coverage/src/coverage/FsmCoverage.scala
@@ -5,7 +5,8 @@
 package coverage
 
 import chisel3.experimental.EnumAnnotations.{EnumComponentAnnotation, EnumDefAnnotation}
-import firrtl.annotations.{Annotation, CircuitTarget, ComponentName, ModuleTarget, Named, ReferenceTarget}
+import coverage.FsmInfoPass.onModule
+import firrtl.annotations.{Annotation, CircuitTarget, ComponentName, ModuleTarget, Named, ReferenceTarget, SingleTargetAnnotation}
 import firrtl._
 import firrtl.options.Dependency
 import firrtl.stage.{Forms, RunFirrtlTransformAnnotation}
@@ -20,7 +21,58 @@ object FsmCoverage {
   )
 }
 
+
+
+
 object FsmCoveragePass extends Transform with DependencyAPIMigration {
+  val Prefix = "f"
+
+  override def prerequisites: Seq[TransformDependency] = Forms.LowForm ++ Seq(Dependency(FsmInfoPass))
+  override def invalidates(a: Transform): Boolean = false
+
+
+  override def execute(state: CircuitState): CircuitState = {
+    // collect FSMs in modules that are not ignored
+    val ignoreMods = Coverage.collectModulesToIgnore(state)
+    val infos = state.annotations.collect{ case a: FsmInfoAnnotation if !ignoreMods(a.target.module) => a }
+
+    // if there are no FSMs there is nothing to do
+    if(infos.isEmpty) return state
+
+
+    val circuit = state.circuit.mapModule(onModule(_, infos))
+
+    state.copy(circuit = circuit)
+  }
+
+  private def onModule(m: ir.DefModule, infos: Seq[FsmInfoAnnotation]): ir.DefModule = m match {
+    case mod: ir.Module =>
+      val fsms = infos.filter(_.target.module == mod.name)
+      if(fsms.isEmpty) { mod } else {
+
+        fsms.foreach(onFsm)
+       // TODO: instrument!
+
+        mod
+      }
+    case other => other
+  }
+
+  private def onFsm(fsm: FsmInfoAnnotation): Unit = {
+    println(s"[${fsm.target.module}.${fsm.target.name}] Found FSM")
+    if(fsm.start.nonEmpty) { println(s"Start: ${fsm.start}") }
+    println("Transitions:")
+    fsm.transitions.foreach(t => println(s"${t._1} -> ${t._2}"))
+
+  }
+}
+
+case class FsmInfoAnnotation(target: ReferenceTarget, states: Seq[(String, BigInt)], transitions: Seq[(String, String)], start: String) extends SingleTargetAnnotation[ReferenceTarget] {
+  override def duplicate(n: ReferenceTarget) = copy(target=n)
+}
+
+/** Annotates FSMs in the design with information about all available states and transitions. */
+object FsmInfoPass extends Transform with DependencyAPIMigration {
   val Prefix = "f"
 
   override def prerequisites: Seq[TransformDependency] = Forms.LowForm
@@ -33,59 +85,50 @@ object FsmCoveragePass extends Transform with DependencyAPIMigration {
     // if there are no enums, we won't be able to find any FSMs
     if(enums.isEmpty) return state
 
-    val newAnnos = mutable.ListBuffer[Annotation]()
     val c = CircuitTarget(state.circuit.main)
-    val ignoreMods = Coverage.collectModulesToIgnore(state)
-    val circuit = state.circuit.mapModule(onModule(_, c, newAnnos, enums, components, ignoreMods))
-    val annos = newAnnos.toList ++ state.annotations
-    CircuitState(circuit, annos)
+    val infos = state.circuit.modules.flatMap(onModule(_, c, enums, components))
+
+    state.copy(annotations = infos ++: state.annotations)
   }
 
-  private case class ModuleCtx(
-    annos:     mutable.ListBuffer[Annotation],
-    namespace: Namespace,
-    m:         ModuleTarget,
-    enums:     Map[String, EnumDefAnnotation],
-    components: Map[String, EnumComponentAnnotation])
+  private def onModule(m: ir.DefModule, c: CircuitTarget, enums: Map[String, EnumDefAnnotation], components: Seq[EnumComponentAnnotation]): List[Annotation] = m match {
+    case mod: ir.Module =>
+      val localComponents = components
+        .filter(c => toReferenceTarget(c.target).module == mod.name)
+        .map(c => toReferenceTarget(c.target).ref -> c).toMap
+      if (localComponents.isEmpty) {
+        List()
+      } else {
+        val enumRegs = new ModuleScanner(localComponents).run(mod)
+        enumRegs.map { case EnumReg(enumTypeName, regDef, next) =>
+          analyzeFSM(c.module(mod.name), regDef, next, enums(enumTypeName).definition)
+        }.toList
+      }
+    case other => List()
+  }
 
-  private def onModule(m: ir.DefModule, c: CircuitTarget, annos: mutable.ListBuffer[Annotation],
-    enums: Map[String, EnumDefAnnotation], components: Seq[EnumComponentAnnotation], ignore: Set[String]): ir.DefModule =
-    m match {
-      case mod: ir.Module if !ignore(mod.name) =>
-        val localComponents = components
-          .filter(c => toReferenceTarget(c.target).module == mod.name)
-          .map(c => toReferenceTarget(c.target).ref -> c).toMap
-        if(localComponents.isEmpty) { mod } else {
-
-          val enumRegs = new ModuleScanner(localComponents).run(mod)
-          enumRegs.foreach { case EnumReg(enumTypeName, regDef, next) =>
-            analyzeFSM(regDef, next, enums(enumTypeName).definition)
-          }
-
-          val namespace = Namespace(mod)
-          namespace.newName(Prefix)
-          val ctx = ModuleCtx(annos, namespace, c.module(mod.name), enums, localComponents)
-
-
-          mod
-        }
-      case other => other
-    }
-
-
-  private def analyzeFSM(regDef: ir.DefRegister, next: ir.Expression, states: Map[String, BigInt]): Unit = {
-    println(s"Analyzing register ${regDef.name}")
-    val clock = regDef.clock
-    println(s"Clock: $clock")
-    println(s"Next: ${next.serialize}")
-    println("Next States:")
+  private def analyzeFSM(module: ModuleTarget, regDef: ir.DefRegister, nextExpr: ir.Expression, states: Map[String, BigInt]): FsmInfoAnnotation = {
+    val (resetState, next) = destructReset(nextExpr)
+    // println(s"Next: ${next.serialize}")
     val intToState = states.toSeq.map{ case (k,v) => v -> k }.toMap
-    val transitions = destructMux(next)
-    transitions.foreach { case (guard, nx) =>
-      val gs = guardStates(guard, regDef.name, intToState).getOrElse(states.keySet)
-      // println(s"${guard.serialize} --> ${nx.serialize}")
-      println(s" ${gs} --> ${nextStates(nx, regDef.name, intToState, gs)}")
-    }
+    val transitions = destructMux(next).flatMap { case (guard, nx) =>
+      val from = guardStates(guard, regDef.name, intToState).getOrElse(states.keySet)
+      val to = nextStates(nx, regDef.name, intToState, from)
+      from.flatMap(f => to.map(t => f -> t))
+    }.sortBy(_._1)
+
+    FsmInfoAnnotation(module.ref(regDef.name),
+      states = states.toSeq.sorted,
+      transitions = transitions,
+      start = resetState.map(intToState).getOrElse("")
+    )
+  }
+
+  // tries to extract the reset value
+  private def destructReset(e: ir.Expression): (Option[BigInt], ir.Expression) = e match {
+    case ir.Mux(ir.Reference("reset", _, _, _), rval: ir.UIntLiteral, oval, _) => (Some(rval.value), oval)
+    case ir.Mux(ir.DoPrim(PrimOps.Not, Seq(ir.Reference("reset", _, _, _)), _, _), oval, rval: ir.UIntLiteral, _) => (Some(rval.value), oval)
+    case _ => (None, e)
   }
 
   private def destructMux(e: ir.Expression): List[(ir.Expression, ir.Expression)] = e match {
@@ -134,7 +177,23 @@ object FsmCoveragePass extends Transform with DependencyAPIMigration {
         case Some(s) => Some(states.values.toSet -- s)
         case None => None
       }
-    case other => None// no states
+    case other =>
+      val symbols = findSymbols(other)
+      if(symbols.contains(name)) {
+        logger.warn("[FSM] over-approximating the states")
+        Some(states.values.toSet)
+      } else { None } // no states
+  }
+
+  private def findSymbols(e: ir.Expression): Seq[String] = e match {
+    case r: ir.Reference => Seq(r.name)
+    case ir.SubField(expr, _, _, _) => findSymbols(expr)
+    case ir.SubIndex(expr, _, _, _) => findSymbols(expr)
+    case ir.SubAccess(expr, index, _, _) => Seq(expr, index).flatMap(findSymbols)
+    case ir.DoPrim(_, args, _, _) => args.flatMap(findSymbols)
+    case ir.Mux(cond, tval, fval, _) => Seq(cond, tval, fval).flatMap(findSymbols)
+    case ir.ValidIf(cond, value, _) => Seq(cond, value).flatMap(findSymbols)
+    case _ => Seq()
   }
 
   private def toReferenceTarget(n: Named): ReferenceTarget = n match {

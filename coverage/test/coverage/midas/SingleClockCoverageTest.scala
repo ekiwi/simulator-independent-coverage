b54c11fba93cb3861a08996ae7e65f31f6e8ecae
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-05-20 17:31:47 -0700

    [coverage] toggle coverage seems to work if we exclude the main module

--- coverage/test/coverage/midas/SingleClockCoverageTest.scala
+++ coverage/test/coverage/midas/SingleClockCoverageTest.scala
@@ -21,13 +21,30 @@ class SingleClockCoverageScanChainTest extends LeanTransformSpec(Seq(Dependency(
 
 
 class SingleClockLineCoverageTest extends LeanTransformSpec(Seq(Dependency(LineCoveragePass), Dependency(CoverageStatisticsPass))) {
-  behavior of "LineCoverage"
+  behavior of "LineCoverage for Single Clock Chipyard Design"
 
   it should "instrument a single clock FireSim design" in {
     val m = CircuitTarget("FireSim").module("FireSim")
     val ll = LogLevel.Warn
     val state = Logger.makeScope(Seq(LogLevelAnnotation(ll))) {
       compile(ClockAnalysisExamples.firesimRocketSingleClock)
+    }
+  }
+}
+
+class SingleClockToggleCoverageTest extends LeanTransformSpec(Seq(Dependency(ToggleCoveragePass), Dependency(CoverageStatisticsPass))) {
+  behavior of "ToggleCoverage for Single Clock Chipyard Design"
+
+  val options = ToggleCoverage.all
+
+  it should "instrument a single clock FireSim design" in {
+    // we do not want to instrument the Firesim top, it will actually fail since that module has no clock input
+    // and our clock finding heuristic isn't good enough to deal with that :(
+    val m = CircuitTarget("FireSim").module("FireSim")
+    val skipTop = Seq(DoNotCoverAnnotation(m))
+    val ll = LogLevel.Warn
+    val state = Logger.makeScope(Seq(LogLevelAnnotation(ll))) {
+      compile(ClockAnalysisExamples.firesimRocketSingleClock, annos = options ++ skipTop)
     }
   }
 }

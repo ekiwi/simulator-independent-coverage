888232f336d0ba61150d88fb11d904b8e826eaa0
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-01-26 19:28:21 -0500

    import afl proxy and setup script

---
+++ fuzzing/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Prepare codebase for fuzzing
+
+set -e
+
+if ! [[ -p a2j ]]; then
+  mkfifo a2j
+fi
+
+if ! [[ -p j2a ]]; then
+  mkfifo j2a
+fi
+
+if ! [[ -d seeds ]]; then
+  mkdir seeds
+fi
+
+cd afl-proxy
+make
+
+echo "SETUP COMPLETE"
+
+exit 0

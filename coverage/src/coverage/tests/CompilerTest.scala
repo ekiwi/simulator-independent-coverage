2f58c8e14d10e6b1610f35236101e3a6aa18f09d
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-02-03 13:32:47 -0500

    wip

--- coverage/src/coverage/tests/CompilerTest.scala
+++ coverage/src/coverage/tests/CompilerTest.scala
@@ -4,7 +4,8 @@ import chisel3.RawModule
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 import chiseltest.ChiselScalatestTester
 import chiseltest.experimental.sanitizeFileName
-import firrtl.options.TargetDirAnnotation
+import firrtl.options.{InputAnnotationFileAnnotation, TargetDirAnnotation}
+import firrtl.stage.{FirrtlFileAnnotation, FirrtlStage}
 import firrtl.{AnnotationSeq, EmittedFirrtlCircuitAnnotation, EmittedFirrtlModuleAnnotation, EmittedVerilogCircuitAnnotation, EmittedVerilogModuleAnnotation}
 import org.scalatest.TestSuite
 
@@ -17,19 +18,37 @@ trait CompilerTest extends ChiselScalatestTester {
 
   protected def compile[M <: RawModule](gen: => M, target: String, a: AnnotationSeq = List(), ll: String = "warn"): (String, AnnotationSeq) = {
     val stage = new ChiselStage
+    val r = stage.execute(Array("-E", target, "-ll", ll), ChiselGeneratorAnnotation(() => gen) +: testRunDir +: a ++: annos)
+    (extractSource(r), r)
+  }
 
-    // ensure that test files don't just end up in the root directory
-    val testName = sanitizeFileName(scalaTestContext.value.get.name)
-    val testRunDir = TargetDirAnnotation("test_run_dir" + File.separator + testName)
+  protected def compileFile(firrtlFile: os.Path, annoFile: os.Path, target: String, a: AnnotationSeq = List(), ll: String = "warn"): (String, AnnotationSeq) = {
+    assert(os.exists(firrtlFile), firrtlFile.toString())
+    assert(os.exists(annoFile), annoFile.toString())
+    val stage = new FirrtlStage
+    val fileAnnos = Seq(FirrtlFileAnnotation(firrtlFile.toString()), InputAnnotationFileAnnotation(annoFile.toString()))
+    val r = stage.execute(Array("-E", target, "-ll", ll), fileAnnos ++: testRunDir +: a ++: annos)
+    (extractSource(r), r)
+  }
 
-    val r = stage.execute(Array("-X", target, "-ll", ll), ChiselGeneratorAnnotation(() => gen) +: testRunDir +: a ++: annos)
-    val src = r.collect {
+
+
+  private def extractSource(annos: AnnotationSeq): String = {
+    val src = annos.collect {
       case EmittedFirrtlCircuitAnnotation(a) => a
       case EmittedFirrtlModuleAnnotation(a) => a
       case EmittedVerilogCircuitAnnotation(a) => a
       case EmittedVerilogModuleAnnotation(a) => a
     }.map(_.value).mkString("")
+    src
+  }
 
-    (src, r)
+
+
+  private def testRunDir: TargetDirAnnotation = {
+    // ensure that test files don't just end up in the root directory
+    val testName = sanitizeFileName(scalaTestContext.value.get.name)
+    val testRunDir = TargetDirAnnotation("test_run_dir" + File.separator + testName)
+    testRunDir
   }
 }

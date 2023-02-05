5b21ffb67d4ea9ec7405aa78ff509bd1f840112c
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-02-04 21:08:42 -0500

    fsm info pass: inline based approach seems to be working

--- coverage/src/coverage/FsmInfoPass.scala
+++ coverage/src/coverage/FsmInfoPass.scala
@@ -24,8 +24,6 @@ object FsmInfoPass extends Transform with DependencyAPIMigration {
   override def prerequisites: Seq[TransformDependency] = Forms.LowForm
   override def invalidates(a: Transform): Boolean = false
 
-  private val debug: Boolean = true
-
   override protected def execute(state: CircuitState): CircuitState = {
     val enums = state.annotations.collect { case a: EnumDefAnnotation => a.typeName -> a }.toMap
     val components = state.annotations.collect { case a : EnumComponentAnnotation => a }
@@ -47,142 +45,38 @@ object FsmInfoPass extends Transform with DependencyAPIMigration {
       if (localComponents.isEmpty) {
         List()
       } else {
-        if(debug) println(mod.serialize)
         // extract net info from module
         val regNames = localComponents.keySet
         val netData = new ModuleNetAnalyzer(regNames).run(mod)
         localComponents.map { case (name, anno) =>
-          analyzeFSM(c.module(mod.name), ???, ???, enums(anno.enumTypeName).definition)
+          analyzeFSM(c.module(mod.name), name, netData, enums(anno.enumTypeName).definition)
         }.toList
       }
     case other => List()
   }
 
-  private def analyzeFSM(module: ModuleTarget, regDef: ir.DefRegister, nextExpr: ir.Expression, states: Map[String, BigInt]): FsmInfoAnnotation = {
-    if(debug) println(s"Analyzing FSM in $module")
+  private def analyzeFSM(module: ModuleTarget, regName: String, netData: ModuleNetData, states: Map[String, BigInt]): FsmInfoAnnotation = {
+    val nextExpr = netData.next(regName)
     val (resetState, next) = destructReset(nextExpr)
-    // if(debug) println(s"Next: ${next.serialize}")
-    val allStates = states.values.toSet
-    val destructedMux = destructMux(next)
-    if(debug) {
-      println(s"Next:\n${next.serialize}")
-      println(s"Destructed:")
-      destructedMux.foreach { case (guard, nx) =>
-        println(s"When ${guard.serialize} ==> ${nx.serialize}")
-      }
-    }
-    val transitions = destructedMux.flatMap { case (guard, nx) =>
-      val from = guardStates(guard, regDef.name, allStates).getOrElse(allStates)
-      val to = nextStates(nx, regDef.name, allStates, from)
-      from.flatMap(f => to.map(t => f -> t))
-    }.sortBy(_._1)
 
-    FsmInfoAnnotation(module.ref(regDef.name),
+    // analyze next state expression for each start state
+    val transitions = states.toSeq.sortBy(_._2).flatMap { case (name, from) =>
+      val res = new FsmAnalyzer(netData.con, regName, from).analyzeNext(next)
+      res.map{ case (_pred, to) =>  from -> to}
+    }
+
+    FsmInfoAnnotation(module.ref(regName),
       states = states.toSeq.sorted.map{ case (n, i) => i -> n },
       transitions = transitions,
       start = resetState
     )
   }
 
-  // tries to extract the reset value
+  // tries to extract the reset value, this assumes synchronous resets!
   private def destructReset(e: ir.Expression): (Option[BigInt], ir.Expression) = e match {
     case ir.Mux(ir.Reference("reset", _, _, _), rval: ir.UIntLiteral, oval, _) => (Some(rval.value), oval)
     case ir.Mux(ir.DoPrim(PrimOps.Not, Seq(ir.Reference("reset", _, _, _)), _, _), oval, rval: ir.UIntLiteral, _) => (Some(rval.value), oval)
     case _ => (None, e)
-  }
-
-  private def destructMux(e: ir.Expression): List[(ir.Expression, ir.Expression)] = e match {
-    case ir.Mux(cond, tval, fval, _) =>
-      val tru = destructMux(tval)
-      val fals = destructMux(fval)
-      tru.map{ case (guard, value) => (Utils.and(cond, guard), value) } ++
-        fals.map{ case (guard, value) => (Utils.and(Utils.not(cond), guard), value) }
-    case other => List((Utils.True(), other))
-  }
-  private def nextStates(e: ir.Expression, name: String, allStates: Set[BigInt], guards: Set[BigInt]): Set[BigInt] = e match {
-    case c: ir.UIntLiteral => Set(c.value)
-    case r: ir.Reference if r.name == name => guards
-    case _ => allStates
-  }
-  private def guardStates(e: ir.Expression, name: String, allStates: Set[BigInt]): Option[Set[BigInt]] = e match {
-    case ir.DoPrim(PrimOps.Eq, Seq(r: ir.Reference, c: ir.UIntLiteral), _, _) if r.name == name =>
-      Some(Set(c.value))
-    case ir.DoPrim(PrimOps.Eq, Seq(c: ir.UIntLiteral, r: ir.Reference), _, _) if r.name == name =>
-      Some(Set(c.value))
-    case ir.DoPrim(PrimOps.Neq, Seq(r: ir.Reference, c: ir.UIntLiteral), _, _) if r.name == name =>
-      Some(allStates -- Set(c.value))
-    case ir.DoPrim(PrimOps.Neq, Seq(c: ir.UIntLiteral, r: ir.Reference), _, _) if r.name == name =>
-      Some(allStates -- Set(c.value))
-    case ir.DoPrim(PrimOps.Or, Seq(a, b), _, _) =>
-      val aStates = guardStates(a, name, allStates)
-      val bStates = guardStates(b, name, allStates)
-      combineOr(aStates, bStates)
-    case ir.DoPrim(PrimOps.And, Seq(a, b), _, _) =>
-      val aStates = guardStates(a, name, allStates)
-      val bStates = guardStates(b, name, allStates)
-      combineAnd(aStates, bStates)
-    case ir.DoPrim(PrimOps.Not, Seq(a), _, _) =>
-      val aStates = guardStates(a, name, allStates)
-      aStates match {
-        case Some(s) => Some(allStates -- s)
-        case None => None
-      }
-    // try to analyze the following pattern
-    // orr(cat(cat(eq(release_state, UInt(9)), eq(release_state, UInt(6))), eq(release_state, UInt(1))))
-    case ir.DoPrim(PrimOps.Orr, Seq(s @ ir.DoPrim(PrimOps.Cat, _, _, _)), _, tpe) =>
-      val bits = getCatBits(s)
-      bits.foreach { b =>
-        assert(firrtl.bitWidth(b.tpe) == 1, s"Cannot deal with concatenated value ${b.serialize}")
-      }
-
-      val sts = bits.map(guardStates(_, name, allStates))
-      if(sts.length == 1) {
-        sts.head
-      } else {
-        sts.reduce(combineOr)
-      }
-    case other =>
-      val symbols = findSymbols(other)
-      if(symbols.contains(name)) {
-        // throw new RuntimeException(s"failed to analyze:\n" + other.serialize)
-        logger.warn("[FSM] over-approximating the states")
-        Some(allStates)
-      } else { None } // no states
-  }
-
-  private def combineOr(aStates: Option[Set[BigInt]], bStates: Option[Set[BigInt]]): Option[Set[BigInt]] = {
-    (aStates, bStates) match {
-      case (None, None) => None
-      case (None, a) => a
-      case (a, None) => a
-      case (Some(a), Some(b)) => Some(a | b)
-    }
-  }
-
-  private def combineAnd(aStates: Option[Set[BigInt]], bStates: Option[Set[BigInt]]): Option[Set[BigInt]] = {
-    (aStates, bStates) match {
-      case (None, None) => None
-      case (None, a) => a
-      case (a, None) => a
-      case (Some(a), Some(b)) => Some(a & b)
-    }
-  }
-
-  private def getCatBits(e: ir.Expression): List[ir.Expression] = e match {
-    case ir.DoPrim(PrimOps.Cat, Seq(msb, lsb), _, _) =>
-      getCatBits(msb) ++ getCatBits(lsb)
-    case other => List(other)
-  }
-
-  private def findSymbols(e: ir.Expression): Seq[String] = e match {
-    case r: ir.Reference => Seq(r.name)
-    case ir.SubField(expr, _, _, _) => findSymbols(expr)
-    case ir.SubIndex(expr, _, _, _) => findSymbols(expr)
-    case ir.SubAccess(expr, index, _, _) => Seq(expr, index).flatMap(findSymbols)
-    case ir.DoPrim(_, args, _, _) => args.flatMap(findSymbols)
-    case ir.Mux(cond, tval, fval, _) => Seq(cond, tval, fval).flatMap(findSymbols)
-    case ir.ValidIf(cond, value, _) => Seq(cond, value).flatMap(findSymbols)
-    case _ => Seq()
   }
 
   private def toReferenceTarget(n: Named): ReferenceTarget = n match {
@@ -192,21 +86,113 @@ object FsmInfoPass extends Transform with DependencyAPIMigration {
 }
 
 
+private class FsmAnalyzer(con: Map[String, ConnectionInfo], stateRegName: String, stateValue: BigInt) {
+  def analyzeNext(e: ir.Expression): Seq[(FsmPred, BigInt)] = {
+    val simplified = simplify(followAll = true)(e)
+    simplified match {
+      case ir.UIntLiteral(value, _) => Seq((FTrue, value)) // the state will be `value` without any condition
+      case ir.Mux(condExpr, tvalExpr, fvalExpr, _) =>
+        // implement branch
+        analyzePred(condExpr) match {
+          case FTrue => analyzeNext(tvalExpr)
+          case FFalse => analyzeNext(fvalExpr)
+          case cond =>
+            val ts = analyzeNext(tvalExpr).map{ case (pred, state) =>  FAnd(cond, pred) -> state}
+            val notCond = FNot(cond)
+            val fs = analyzeNext(fvalExpr).map{ case (pred, state) =>  FAnd(notCond, pred) -> state}
+            ts ++ fs
+        }
+      case other => throw new NotImplementedError(s"Cannot analyze next state expression: ${other.serialize}")
+    }
+  }
+  def analyzePred(e: ir.Expression): FsmPred = {
+    require(firrtl.bitWidth(e.tpe) == 1, s"Unexpected non-bool expression ${e.serialize}")
+    simplify(followAll = false)(e) match { // predicates that do not depend on the state do not matter
+      case ir.UIntLiteral(value, _) => if(value == 1) { FTrue } else { FFalse }
+      case ir.DoPrim(PrimOps.And, Seq(a, b), _, _) =>
+        FAnd(analyzePred(a), analyzePred(b))
+      case ir.DoPrim(PrimOps.Or, Seq(a, b), _, _) =>
+        FOr(analyzePred(a), analyzePred(b))
+      case ir.DoPrim(PrimOps.Not, Seq(a), _, _) =>
+        FNot(analyzePred(a))
+      case other if !exprDependsOnState(other) => FAtom(other.serialize)
+      case other => throw new NotImplementedError(s"Cannot analyze next state expression: ${other.serialize}")
+    }
+  }
+  // this assumes that all relevant references have been inline!
+  private def exprDependsOnState(e: ir.Expression): Boolean = e match {
+    case ir.Reference(name, _, _, _) if name == stateRegName => true
+    case other => getChildren(other).exists(exprDependsOnState)
+  }
+  def simplify(followAll: Boolean)(e: ir.Expression): ir.Expression = {
+    // we simplify bottom up!
+    e.mapExpr(simplify(followAll)) match {
+      // replace references to the state register with the state value
+      case ir.Reference(name, ir.UIntType(width), _, _) if name == stateRegName =>
+        ir.UIntLiteral(stateValue, width)
+      // follow (i.e., inline) some references
+      case r@ir.Reference(name, _, _, _) =>
+        con.get(name) match {
+          case None => r // nothing to do, cannot follow
+          case Some(info) =>
+            val dependsOnStateValue = info.registerDependencies.contains(stateRegName)
+            if (dependsOnStateValue || followAll) {
+              simplify(followAll)(info.expr)
+            } else {
+              r
+            }
+        }
+      case other =>
+        // try to propagate any constants
+        propConst(other)
+    }
+  }
+}
+
+private object propConst {
+  private def toUInt(cond: Boolean): ir.Expression = if(cond) { Utils.True() } else { Utils.False() }
+  // performs a single level of constant propagation (does not recurse into expression!)
+  def apply(e: ir.Expression): ir.Expression = e match {
+    case ir.Mux(ir.UIntLiteral(value, _), tval, fval, _) => if(value == 1) { tval } else { fval }
+    case ir.DoPrim(PrimOps.Eq, Seq(ir.UIntLiteral(a, _), ir.UIntLiteral(b, _)), _, _) => toUInt(a == b)
+    case other => other
+  }
+}
+
+private trait FsmPred
+private case class FAnd(a: FsmPred, b: FsmPred) extends FsmPred {
+  override def toString = s"($a && $b)"
+}
+private case class FOr(a: FsmPred, b: FsmPred) extends FsmPred {
+  override def toString = s"($a || $b)"
+}
+private case class FNot(a: FsmPred) extends FsmPred {
+  override def toString = s"!$a"
+}
+private case class FAtom(name: String) extends FsmPred  {
+  override def toString = name
+}
+private case object FTrue extends FsmPred {
+  override def toString = "True"
+}
+private case object FFalse extends FsmPred {
+  override def toString = "False"
+}
 
 /** Contains the right-hand-side expression and transitive register dependency for a single node or register next expression. */
 private case class ConnectionInfo(expr: ir.Expression, registerDependencies: Set[String])
 /** Contains information about all nodes and register next connections in the circuit */
-private case class ModuleNetData(con: Map[String, ConnectionInfo])
+private case class ModuleNetData(con: Map[String, ConnectionInfo], next: Map[String, ir.Expression])
 private class ModuleNetAnalyzer(registers: Set[String]) {
   private val con = mutable.HashMap[String, ConnectionInfo]()
+  private val next = mutable.HashMap[String, ir.Expression]()
   def run(mod: ir.Module): ModuleNetData = {
     mod.foreachStmt(onStmt)
-    ModuleNetData(con.toMap)
+    ModuleNetData(con.toMap, next.toMap)
   }
   def onStmt(s: ir.Statement): Unit = s match {
     case ir.Connect(_, ir.Reference(name, _, kind, _), expr) if kind == RegKind =>
-      // register do not induce any dependencies, since we are only looking at a single cycle!
-      con(name) = ConnectionInfo(expr, Set())
+      next(name) = expr
     case ir.Connect(_, loc, expr) =>
       con(loc.serialize) = ConnectionInfo(expr, findDeps(expr))
     case ir.DefNode(_, name, expr) =>
@@ -218,10 +204,19 @@ private class ModuleNetAnalyzer(registers: Set[String]) {
       if(registers.contains(name)) { Set(name) } else {
         con.get(name).map(_.registerDependencies).getOrElse(Set())
       }
-    case _ : ir.UIntLiteral | _ : ir.SIntLiteral => Set()
-    case prim : ir.DoPrim => prim.args.map(findDeps).reduce(_ | _)
-    case ir.Mux(cond, tval, fval, _) => Seq(cond, tval, fval).map(findDeps).reduce(_ | _)
-    case ir.ValidIf(cond, value, _) => Seq(cond, value).map(findDeps).reduce(_ | _)
-    case _ : ir.RefLikeExpression => Set() // ref-like but not a reference => cannot be a scalar register
+    case other => getChildren(other).map(findDeps) match {
+      case Seq() => Set()
+      case children => children.reduce(_ | _)
+    }
+  }
+}
+
+private object getChildren {
+  def apply(e: ir.Expression): Seq[ir.Expression] = e match {
+    case _ : ir.UIntLiteral | _ : ir.SIntLiteral | _: ir.Reference => Seq()
+    case prim: ir.DoPrim => prim.args
+    case ir.Mux(cond, tval, fval, _) => Seq(cond, tval, fval)
+    case ir.ValidIf(cond, value, _) => Seq(cond, value)
+    case ir.SubField(expr, _, _, _) => Seq(expr)
   }
 }
